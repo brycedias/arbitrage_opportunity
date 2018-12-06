a5fa65c25d081506a796fae830f96ef7070b5c8b
Carter Alexander <alecart@lycoming.edu>
2018-12-06 16:27:18 -0500

    floydWarshall

--- javascripts/floydwarshall.js
+++ javascripts/floydwarshall.js
@@ -1,50 +1,30 @@
-/*
-
-
-Bryce and I took implemented the Floyd-Warshall algorithm
-based on the algorithm below:
-
-
-
-let V = number of vertices in graph
-let dist = V * V array of minimum distances
-for each vertex v
-	dist[v][v] <- 0
-for each edge(u,v)
-	dist[u][v] <- weight(u,v)
-for k from 1 to V
-	for i from 1 to V
-		for j from 1 to V
-			if dist[i][j] > dist[i][k] + dist[k][j]
-				dist[i][j] <- dist[i][k] + dist[k][j]
-			end if
-*/
-
-function floydWarshall(G) {
-  let V = G.adjMatrix.length
+ // Floyd-Warshall Algorithm
+ // graph object contains currencies.length, ratesObject, currencies
+function floydWarshall(G) { 
+// creation of  5-by-5 adjMatrix
+  let V = G.adjMatrix.length 
   let dist = new Array(V)
   for (let i = 0; i < V; i++) {
     dist[i] = new Array(V)
   }
-  //debugger
-  //console.log('first dist', dist)
+
+  // initializing each of the indices to 0
   for (let v = 0; v < V; v++) {
     dist[v][v] = 0
   }
-  //debugger
-  //console.log('second dist', dist)
+
+  // initializing each of the rates of dist to the rates of the graph's adjMatrix
   for (let u = 0; u < V; u++) {
     for (let v = 0; v < V; v++) {
       dist[u][v] = G.adjMatrix[u][v]
     }
   }
-  debugger
   
-  console.log('%cOrder of Currency Arrays: EUR, GBP, CAD, USD, JPY', 'color: green')
-  console.log('%cG.adjMatrix (Natural log of original rates)', 'color: red;')
-  console.log(G.adjMatrix)
-  //console.log('third dist', dist)
-  
+  // Floyd-Warshall Algorithm
+	// progress through each distance between each of the vertices
+	// and if the distance between the vertices is shorter betweeen
+	// a combination of two edge pathways than the first,
+	// then it becomes the new shortest distance between the vertices
   for (let k = 1; k < V; k++){
 	  for (let i = 1; i < V; i++){
 		  for (let j = 1; j < V; j++){
@@ -54,14 +34,10 @@ function floydWarshall(G) {
 		  }
 	  }
   }
-  debugger
-  console.log('%cOrder of Currency Arrays: EUR, GBP, CAD, USD, JPY', 'color: green')
-  console.log('%cshortest distances found between all points', 'color: blue;')
-  console.log(dist)
- 
-
 }
 
+// hardcoded function whose shortest path is based on
+// the baseCurrency selected
 function demoShortestPath(base) {
   switch (base) {
     case 'EUR':
@@ -75,7 +51,6 @@ function demoShortestPath(base) {
     case 'JPY':
       return [4, 0, 3, 4]
   }
-  
 }
 
 
